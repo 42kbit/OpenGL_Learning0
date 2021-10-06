221ee4c2c4f3421d041b45807e7d1d7d74e5f433
Ostrikov Serhii <serostrikov@gmail.com>
2021-10-06 15:34:27 +0300

    Added GLAD

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
@@ -15,15 +18,37 @@ int main(void)
         glfwTerminate();
         return -1;
     }
-
-    /* Make the window's context current */
+	/* Make the window's context current */
     glfwMakeContextCurrent(window);
-
+	
+	if(!gladLoadGL())
+	{
+		std::cout << "Cannot load GLAD" << std::endl;
+		exit(-1);
+	}
+	
+	float verts[] = {0,0, 1,0, 0,1};
+	
+	uint32_t vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+		
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, nullptr);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        //glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0,1,0,1);
+        glClear(GL_COLOR_BUFFER_BIT);
+		
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glEnableVertexAttribArray(0);
+			glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDisableVertexAttribArray(0);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
